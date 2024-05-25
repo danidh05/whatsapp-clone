fcc77abd960f309a893268a73c1d8b8e024b7f55
danidh05 <150284540+danidh05@users.noreply.github.com>
2024-05-25 03:09:19 +0300

    Gained intro about convex

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "../src/providers/theme-provider";
 import "./globals.css";
+import ConvexClientProvider from "@/src/providers/convex-client-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +20,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <ThemeProvider attribute='class' defaultTheme='system' enableSystem disableTransitionOnChange>
-          {children}
+          <ConvexClientProvider>
+            {children}
+          </ConvexClientProvider>
         </ThemeProvider>
       </body>
     </html>
