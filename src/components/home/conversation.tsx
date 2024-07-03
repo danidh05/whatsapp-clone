7016dabd7b9afda43050905f6dcb476aa3491093
danidh05 <150284540+danidh05@users.noreply.github.com>
2024-07-03 21:08:26 +0300

    getGroupMembers query added

--- src/components/home/conversation.tsx
+++ src/components/home/conversation.tsx
@@ -4,6 +4,7 @@ import { MessageSeenSvg } from "@/lib/svgs"; // Importing MessageSeenSvg compone
 import { ImageIcon, Users, VideoIcon } from "lucide-react"; // Importing icons from lucide-react library
 import { useQuery, } from "convex/react";
 import { api } from "../../../convex/_generated/api";
+import { useConversationStore } from "@/store/chat-store";
 // Conversation Component Definition
 const Conversation = ({ conversation }: { conversation: any }) => {
     // Destructuring conversation data
@@ -12,10 +13,16 @@ const Conversation = ({ conversation }: { conversation: any }) => {
     const lastMessage = conversation.lastMessage; // Extracting last message from conversation
     const lastMessageType = lastMessage?.messageType; // Extracting message type from last message
     const me = useQuery(api.users.getMe);
+
+    const { setSelectedConversation, selectedConversation } = useConversationStore();
+    const activeBgClass = selectedConversation?._id === conversation._id;
     return (
         // Conversation JSX
         <>
-            <div className={`flex gap-2 items-center p-3 hover:bg-chat-hover cursor-pointer `}>
+            <div
+                className={`flex gap-2 items-center p-3 hover:bg-chat-hover cursor-pointer ${activeBgClass ? "bg-gray-tertiary" : ""} `}
+                onClick={() => setSelectedConversation(conversation)}
+            >
                 <Avatar className='border border-gray-900 overflow-visible relative'> {/* Avatar container */}
                     {conversation.isOnline && ( // Render green dot if user is online
                         <div className='absolute top-0 right-0 w-2.5 h-2.5 bg-green-500 rounded-full border-2 border-foreground' />
